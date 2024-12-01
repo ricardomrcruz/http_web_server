0e9a3cef2d70f3ce357af58aabf61485f54f68a9
ricardomrcruz <strasbourgwebsolutions@gmail.com>
2024-12-01 23:55:55 +0100

    TCP socket server

--- main.cpp
+++ main.cpp
@@ -28,6 +28,28 @@ int main() {
 	hint.sin_port = htons(8080);				// Port 8080
 	hint.sin_addr.S_un.S_addr = INADDR_ANY;		//Binds to any local address
 
+	if (bind(listening, (sockaddr*)&hint, sizeof(hint)) == SOCKET_ERROR) {
+		std::cerr << "Can't bind socket! Error: " << WSAGetLastError() << std::endl;
+		closesocket(listening);
+		WSACleanup();
+		return 1;
+	}
+
+	//Tell Winsock the socket is for listening
+	if (listen(listening, SOMAXCONN) == SOCKET_ERROR) {
+		std::cerr << "Can't listen! Error:" << WSAGetLastError() << std::endl;
+		closesocket(listening);
+		WSACleanup();
+		return 1;
+	}
+
+	std::cout << "Server is listening on port 8000..." << std::endl;
+
+	//cleanup
+	closesocket(listening);
+	WSACleanup();
+	return 0;
+
 
 
 }
