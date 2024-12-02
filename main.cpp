bf95a0ab4a49d93efc0e05824110cae5d3d5ee44
ricardomrcruz <strasbourgwebsolutions@gmail.com>
2024-12-02 13:53:01 +0100

    .

--- main.cpp
+++ main.cpp
@@ -38,14 +38,66 @@ int main() {
 	//Tell Winsock the socket is for listening
 	if (listen(listening, SOMAXCONN) == SOCKET_ERROR) {
 		std::cerr << "Can't listen! Error:" << WSAGetLastError() << std::endl;
+		//closesocket(listening);
+		//WSACleanup();
+		return 1;
+	}
+
+	std::cout << "Server is listening on port 8000...\n";
+
+	//wait for connection
+	sockaddr_in client;
+	int clientSize = sizeof(client);
+
+	SOCKET clientSocket = accept(listening, (sockaddr*)&client, &clientSize);
+	if (clientSocket == INVALID_SOCKET) {
+		std::cerr << "Can't accept client!" << std::endl;
 		closesocket(listening);
 		WSACleanup();
 		return 1;
 	}
 
-	std::cout << "Server is listening on port 8000..." << std::endl;
+	char host[NI_MAXHOST];        // Client's remote name
+	char service[NI_MAXSERV];     // Service (i.e.port) the client is connected on
+
+	ZeroMemory(host, NI_MAXHOST);
+	ZeroMemory(service, NI_MAXSERV);
+
+	if (getnameinfo((sockaddr*)&client, sizeof(client), host, NI_MAXHOST, service, NI_MAXSERV, 0) == 0) {
+		std::cout << "Client connected on port" << service << std::endl;
+	}
+
+	// buffer for receiving client data
+	char buf[4096];
+	ZeroMemory(buf, 4096);
+
+	//recieve client's HTTP request
+	int bytesReceived = recv(clientSocket, buf, 4096, 0);
+	if (bytesReceived == SOCKET_ERROR) {
+		std::cerr << "Error in recv()" << std::endl;
+	}
+	else {
+
+		//Print the HTTP request
+		std::cout << "\nIncoming HTTP Request:\n" << std::string(buf, 0, bytesReceived) << std::endl;
+
+
+		// Create a basic HTTP response
+		std::string response = "HTTP/1.1 200 OK\r\n";
+		response += "Content-Type: text/html\r\n";
+		response += "Connection: close\r\n";
+		response += "\r\n";
+		response += "<html><body><h1>Welcome to your HTTP server!</h1></body></html>";
+
+		//Send the response
+		send(clientSocket, response.c_str(), response.size(), 0);
+
+	}
+
+	
 
 	//cleanup
+	closesocket(clientSocket);
 	closesocket(listening);
 	WSACleanup();
 	return 0;
